54ab7456ebf695a94b0c0ece49912ec339c346f8
RomanPalna <roma.palna@gmail.com>
2021-12-06 16:20:08 +0200

    first calculator is done )

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const refs = {
   buttonSubmit: document.querySelector(".calculator__form"),
-  navButton: document.querySelector(".calculator__nav--item"),
   resultAvarage: document.querySelector(".avarage"),
   resultCost: document.querySelector(".cost"),
   resultTotalCost: document.querySelector(".totalCost"),
   clear: document.querySelector(".button__clear"),
+  toggleResult: document.querySelector(".result"),
 };
 
 refs.buttonSubmit.addEventListener("submit", onInputSubmit);
@@ -28,20 +28,20 @@ function onInputSubmit(event) {
 
 function averageСonsumption(gasoline, distance, gasolineCost) {
   const avarage = (gasoline / distance) * 100;
-  console.log("Середній розхід: ", avarage);
   refs.resultAvarage.innerText = avarage;
 
   const cost = (gasolineCost * avarage) / 100;
-  console.log("Середня вартість: ", cost);
   refs.resultCost.innerText = cost;
 
   const totalCost = cost * distance;
-  console.log("Середня вартість: ", totalCost);
   refs.resultTotalCost.innerText = totalCost;
+
+  refs.toggleResult.style.display = "block";
 }
 
 function handleClear() {
   refs.resultAvarage.innerText = 0;
   refs.resultCost.innerText = 0;
   refs.resultTotalCost.innerText = 0;
+  refs.toggleResult.style.display = "none";
 }
